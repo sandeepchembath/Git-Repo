27c5767543075bd5bb4d7c95cf24e3d373b01306
Sandeep Chembath <sandeepchembath@gmail.com>
2024-03-17 15:13:31 +0000

    Added defensive programming to avoid zero division error.

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 def calculation(x, y, operator):
-    if operator == '+':
-        total = x + y
-        return total
-    elif operator == '-':
-        total = x - y
-        return total
-    elif operator == "x":
-        total = x * y
-        return total
-    elif operator == '/':
-        total = x / y
-        return total
+    
+    try:
+        if operator == '+':
+            total = x + y
+            return total
+        elif operator == '-':
+            total = x - y
+            return total
+        elif operator == "x":
+            total = x * y
+            return total
+        elif operator == '/':
+            total = x / y
+            return total
+        
+    except ZeroDivisionError:
+        print("Division by zero is invalid!!!")
     
 num1 = float(input("Enter a number : "))
 num2 = float(input("Enter a number : "))
 
-operator = input("Enter the operator you would like to use : ")
+ops = ['+', '-', 'x', '/']
+while True:
+    operator = input("Enter the operator you would like to use : ")
+
+    if operator not in ops:
+        print('''Your selected operator is invalid.
+              Please use one of the following symbols :
+                +(addition), -(subtraction), x(multiplication), /(division)''')
+        continue
+
+    else:
+        print("Calculating...")
+        break
+
 
 calc = calculation(num1, num2, operator)
 print(calc)
